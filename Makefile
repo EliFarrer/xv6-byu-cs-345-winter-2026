bca87c19e0fe559b82f77377b6a59c598bc39124
Eli Farrer <eli.farrer@gmail.com>
2026-01-16 22:54:31 -0700

    added complete xargs

--- Makefile
+++ Makefile
@@ -198,6 +198,7 @@ UPROGS=\
 	$U/_sleep\
 	$U/_pingpong\
 	$U/_find\
+	$U/_xargs\
 
 
 
--- user/xargs.c
+++ user/xargs.c
@@ -2,7 +2,7 @@
 #include "user/user.h"
 
 int 
-getToNewline(char* buf, int start)
+getLine(char* buf, int start)
 {
     // returns how many bytes to read (includes a spot for a null terminator)
     if (start >= strlen(buf)) {
@@ -18,29 +18,15 @@ getToNewline(char* buf, int start)
 }
 
 int
-splitSpace(char* line, char** args) {
-    printf("In split space\n");
-    printf("\tString: |%s|\n", line);
+splitLine(char* line, char** args) {
     int argCount = 0;
     int startOfStr = 0;
-    printf("Entering for loop\n");
-    for (int i = 0; i <= strlen(line); i++) {
-        printf("\ti=%d\n", i);
-        char out[2];
-        out[0] = line[i];
-        out[1] = '\0';
-        printf("\tstr[i]=%s\n", out);
+    int newLength = strlen(line);
+    for (int i = 0; i <= newLength; i++) {
         if (line[i] == ' ' || line[i] == '\0') {
-            printf("\tin if statement\n");
-            char arg[60];   // max arg length
-            memmove(arg, line + startOfStr, i - startOfStr); // move from the start string pointer to the args
-            printf("\tmade past mem move\n");
-            arg[i] = '\0';  // set null terminator
-            printf("\targ: %s\n", arg);
-            args[argCount] = arg;
-            printf("\targs: %s\n", args[argCount]);
-            argCount++;
-            startOfStr = i + 1; // reset start string pointer
+            line[i] = '\0';
+            args[argCount++] = line + startOfStr;
+            startOfStr = i + 1; // skip the new null terminator
         }
     }
     return argCount;
@@ -54,31 +40,42 @@ main(int argc, char *argv[])
         fprintf(2, "xargs: error reading stdin\n");
         exit(1);
     }
+    char* command = argv[1];
+    int commandLength = 1;
+    argv += commandLength;
 
     int start = 0;
-    printf("calling get to new line\n");
-    int len = getToNewline(buf, start);
+    int len = getLine(buf, start);
     int count = 0;
     while (len != -1) { // iterate over each stdin line
-        // get the substring
-        char line[60];  // max line length
-        memcpy(line, buf + start, len); // copy from the buffer + start to the line memory
-        line[len]= '\0';
-        printf("length: %d\n", len);
-        printf("got line |%s|\n", line);
-        char* args[100]; // max 100 arguments
-        printf("calling split space\n");
-        int writtenArgs = splitSpace(line, args);
-
-        for (int i = 0; i < writtenArgs; i++) {
-            printf("Got line %d parameter %d: %s\n", count, i, args[i]);
-        }
-
-
         
-        start += len + 1; // move start to next character after newline
-        len = getToNewline(buf, start);
-        count++;
+        int pid = fork();
+        if (pid == 0) { // if child
+            // get the substring
+            char line[60];  // max line length
+            memcpy(line, buf + start, len); // copy from the buffer + start to the line memory
+            line[len]= '\0';
+
+            char* args[100]; // max 100 arguments
+
+            // copy the xargs arguments over
+            int newArgc = argc - commandLength;
+            for (int i = 0; i < newArgc; i++) {
+                args[i] = argv[i];
+            }
+
+            // copy the line args over
+            splitLine(line, args + newArgc);    // pass in the pointer commandLength ahead
+
+            exec(command, args);
+            printf("xargs: failed exec\n");
+        } else {        // if parent
+            wait((int *) 0);
+            // continue
+            start += len + 1; // move start to next character after newline
+            len = getLine(buf, start);
+            count++;
+        }
     }
     // while we read from stdin
         // if it is a newline, fork
