6bfa6926b131d9ef9acb3863837311fd1b9e0e73
Eli Farrer <eli.farrer@gmail.com>
2026-01-24 21:57:41 -0700

    try number 1

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,16 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
+  // attack stack lives at 84215045
+
+  int amount = 32;
+  for (int i = 0; i < 1000; i++) {
+    int* start = (int*) 0x0;
+    char tmp[amount];
+    memmove(tmp, start + (amount * i), amount);
+    printf("Old start: %p, new start: %p: %d\n", start, (start + amount * i), *start);
+  }
+  // // write(2, secret, 8);
 
   exit(1);
 }
