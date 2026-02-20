615b6c2a55367119d4537020931a7bf2abf36827
Eli Farrer <eli.farrer@gmail.com>
2026-02-19 20:39:05 -0700

    added a way to free the page

--- kernel/proc.c
+++ kernel/proc.c
@@ -158,6 +158,11 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+
+  // free the page containing the pid
+  uint64 pa = walkaddr(p->pagetable, (uint64)USYSCALL);
+  kfree((void*)pa);
+
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
